e7bc46b39f9e78b22abeaab05987c1863676f2c3
Vgnttv <o.vignani@gmail.com>
2019-05-10 09:39:42 +0200

    fraudrisk controller

--- client/src/components/login/LoginPage.js
+++ client/src/components/login/LoginPage.js
@@ -1,37 +1,46 @@
-import React, {PureComponent} from 'react'
-import {connect} from 'react-redux'
-import {login} from '../../actions/users'
-import LoginForm from './LoginForm'
-import {Redirect} from 'react-router-dom'
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import { login } from "../../actions/users";
+import LoginForm from "./LoginForm";
+import { Redirect } from "react-router-dom";
+import { Animated } from "react-animated-css";
 
 class LoginPage extends PureComponent {
-	handleSubmit = (data) => {
-		this.props.login(data.email, data.password)
-	}
+  handleSubmit = data => {
+    this.props.login(data.email, data.password);
+  };
 
-	render() {
-		if (this.props.currentUser) return (
-			<Redirect to="/" />
-		)
+  render() {
+    if (this.props.currentUser) return <Redirect to="/" />;
 
-		return (
-			<div className="login-form">
-				
+    return (
+      <div className="login-form">
+        <LoginForm onSubmit={this.handleSubmit} />
 
-				<LoginForm onSubmit={this.handleSubmit} />
-
-        { this.props.error && 
-          <span style={{color:'red'}}>{this.props.error}</span> }
-			</div>
-		)
-	}
+        {this.props.error && (
+          <span className="NO" style={{ color: "red" }}>
+					 <Animated animationIn="zoomIn" animationOut="fadeOut" isVisible={true}>
+            <img
+              className="gif"
+              src="https://media.giphy.com/media/12XMGIWtrHBl5e/giphy.gif"
+            /> <br/>
+						{this.props.error}
+						</Animated>
+          </span>
+        )}
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = function (state) {
-	return {
-		currentUser: state.currentUser,
+const mapStateToProps = function(state) {
+  return {
+    currentUser: state.currentUser,
     error: state.login.error
-	}
-}
+  };
+};
 
-export default connect(mapStateToProps, {login})(LoginPage)
+export default connect(
+  mapStateToProps,
+  { login }
+)(LoginPage);
--- client/src/components/signup/SignupPage.js
+++ client/src/components/signup/SignupPage.js
@@ -1,35 +1,51 @@
-import React, {PureComponent} from 'react'
-import {connect} from 'react-redux'
-import {signup} from '../../actions/users'
-import SignupForm from './SignupForm'
-import {Redirect} from 'react-router-dom'
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import { signup } from "../../actions/users";
+import SignupForm from "./SignupForm";
+import { Redirect } from "react-router-dom";
+import { Animated } from "react-animated-css";
 
 class SignupPage extends PureComponent {
-	handleSubmit = (data) => {
-		this.props.postSignup(data.email, data.password)
-	}
+  handleSubmit = data => {
+    this.props.postSignup(data.email, data.password);
+  };
 
-	render() {
-		if (this.props.signup.success) return (
-			<Redirect to="/" />
-		)
+  render() {
+    if (this.props.signup.success) return <Redirect to="/" />;
 
-		return (
-			<div className="signup-form">
-				<h2>Sign up</h2>
+    return (
+      <div className="signup-form">
+        <h2>Sign up</h2>
 
-				<SignupForm onSubmit={this.handleSubmit} />
-
-				<p style={{color:'red'}}>{ this.props.signup.error }</p>
-			</div>
-		)
-	}
+        <SignupForm onSubmit={this.handleSubmit} />
+        {this.props.signup.error && (
+          <p style={{ color: "red" }}>
+            <Animated
+              animationIn="zoomIn"
+              animationOut="fadeOut"
+              isVisible={true}
+            >
+              <img
+                className="gif"
+                src="https://media.giphy.com/media/12XMGIWtrHBl5e/giphy.gif"
+              />{" "}
+              <br />
+              {this.props.signup.error}
+            </Animated>
+          </p>
+        )}
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = function (state) {
-	return {
-		signup: state.signup
-	}
-}
+const mapStateToProps = function(state) {
+  return {
+    signup: state.signup
+  };
+};
 
-export default connect(mapStateToProps, {postSignup: signup})(SignupPage)
+export default connect(
+  mapStateToProps,
+  { postSignup: signup }
+)(SignupPage);
